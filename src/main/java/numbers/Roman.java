24b174c97f9ece4a59e873ef07e31f246b584180
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2019-02-17 16:00:33 +0100

    extract a class

---
+++ src/main/java/numbers/Roman.java
@@ -0,0 +1,47 @@
+package numbers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Roman {
+	private Map<String, Integer> values = values();
+	private String roman;
+	private Roman(String roman) {
+		this.roman = roman;
+	}
+	private int toAlgebraic() {
+		if (roman.isEmpty()) {
+			return 0;
+		}
+		if (startsAscending()) {
+			return toAlgebraic_startsAscending();
+		}
+		return toAlgebraic_defaultCase();
+	}
+
+	private boolean startsAscending() {
+		return roman.length() > 1 && values.containsKey(roman.substring(0, 2));
+	}
+
+	private int toAlgebraic_startsAscending() {
+		return values.get(roman.substring(0, 2)) + toAlgebraic(roman.substring(2));
+	}
+
+	private int toAlgebraic_defaultCase() {
+		return values.get(roman.substring(0, 1)) + toAlgebraic(roman.substring(1));
+	}
+
+	private Map<String, Integer> values() {
+		Map<String, Integer> values = new HashMap<>();
+		values.put("I", 1);
+		values.put("V", 5);
+		values.put("X", 10);
+		//
+		values.put("IV", 4);
+		values.put("IX", 9);
+		return values;
+	}
+	public static int toAlgebraic(String roman) {
+		return new Roman(roman).toAlgebraic();
+	}
+}
--- src/test/java/numbers/TestApp.java
+++ src/test/java/numbers/TestApp.java
@@ -1,11 +1,7 @@
 package numbers;
 
-import static org.junit.Assert.assertThat;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
@@ -41,39 +37,7 @@ public class TestApp {
 		assertThat(toAlgebraic("XXIX"), is(29));
 	}
 
-	private Map<String, Integer> values = values();
-	private String roman;
-	private int toAlgebraic(String roman) {
-		this.roman = roman;
-		if (roman.isEmpty()) {
-			return 0;
-		}
-		if (startsAscending()) {
-			return toAlgebraic_startsAscending();
-		}
-		return toAlgebraic_defaultCase();
-	}
-
-	private boolean startsAscending() {
-		return roman.length() > 1 && values.containsKey(roman.substring(0, 2));
-	}
-
-	private int toAlgebraic_startsAscending() {
-		return values.get(roman.substring(0, 2)) + toAlgebraic(roman.substring(2));
-	}
-
-	private int toAlgebraic_defaultCase() {
-		return values.get(roman.substring(0, 1)) + toAlgebraic(roman.substring(1));
-	}
-
-	private Map<String, Integer> values() {
-		Map<String, Integer> values = new HashMap<>();
-		values.put("I", 1);
-		values.put("V", 5);
-		values.put("X", 10);
-		//
-		values.put("IV", 4);
-		values.put("IX", 9);
-		return values;
+	private Integer toAlgebraic(String roman) {
+		return Roman.toAlgebraic(roman);
 	}
 }
