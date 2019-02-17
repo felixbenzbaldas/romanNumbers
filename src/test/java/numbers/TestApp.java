86c9971cd11c2bd386a03d61aae608b772f156a0
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2019-02-17 15:19:14 +0100

    one

--- src/test/java/numbers/TestApp.java
+++ src/test/java/numbers/TestApp.java
@@ -1,11 +1,18 @@
 package numbers;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 import org.junit.Test;
 
 public class TestApp {
+	
 	@Test
-	public void testInstallation() {
-		
+	public void one() {
+		assertThat(toAlgebraic("I"), is(1));
 	}
 
+	private int toAlgebraic(String roman) {
+		return 1;
+	}
 }
